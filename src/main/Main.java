0cebcc0311a40073a965c91ac95fca0cc8549a28
Zettajump <zettajump@yahoo.fr>
2016-04-17 17:40:42 +0200

    Eclipse OK

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,13 @@
 package main;
 
+import java.util.Arrays;
+import java.util.Scanner;
 
 import ship.Direction;
 
 public class Main {
 
 	public static void main(String[] args) {
-		super();
 		// TODO Auto-generated method stub
 		Partie p = new Partie();
 		
